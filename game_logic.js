e66a9b0c7e30f41a5e6a834ecfda2a050b16b9db
Pho3b <andrea.rinaldi54@gmail.com>
2020-06-13 21:41:39 +0200

    Major code formatting / Added 10 best records list

--- game_logic.js
+++ game_logic.js
@@ -1,10 +1,9 @@
 const canvas = document.getElementById("snakeCanvas");
 const context = canvas.getContext("2d");
-var randomPos = [];
-randomPos = populateRandomPos(10);
+var randomPos = populateRandomPos(10);
 var current_points = 1;
 var tailPieces = [];
-var gameState = true; //Run or stop the game
+var isGameRunning = true; //Run or stop the game
 var game_starting = 0;
 var can_press_key = true;
 let records = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -12,10 +11,8 @@ const displayPointsElement = document.getElementById('points');
 const recordListElements = document.getElementsByClassName('record_list_element');
 
 
-
 //Food Class
 function Food(width, height, posX, posY) {
-
     this.width = width;
     this.height = height;
     this.posX = posX;
@@ -36,21 +33,18 @@ function Food(width, height, posX, posY) {
 
     // Check for collision with snake
     this.collisionDetection = function () {
-        if (this.posX == snake.PosX && this.posY == snake.posY) {
+        if (this.posX === snake.posX && this.posY === snake.posY) {
             this.randomSpawn();
         }
     };
 
     this.disappear = function () {
-        //Faccio scomparire temporaneamente il cibo
         context.fillStyle = "white";
         context.fillRect(this.posX, this.posY, this.width, this.height);
     }
 
 }
 
-
-// Snake Class
 function Snake(width, height, posX, posY) {
     this.width = width;
     this.height = height;
@@ -100,7 +94,6 @@ function Snake(width, height, posX, posY) {
                     }
                     break;
                 case 37:
-                    //Sinistra
                     if (snake.direction !== 'right') {
                         snake.direction = 'left';
                     }
@@ -117,15 +110,12 @@ function Snake(width, height, posX, posY) {
                     break;
             }
         }
-    }
-
+    };
 
     this.draw = function () {
-        //Salvo la posizione precedente di Snake
         this.prevPosition.posX = this.posX;
         this.prevPosition.posY = this.posY;
         this.prevPosition.direction = this.direction;
-        // console.log('Prima:' +  this.posX, this.posY,this.direction);
 
         switch (this.direction) {
             case 'left':
@@ -141,7 +131,6 @@ function Snake(width, height, posX, posY) {
                 this.posY += this.increment;
                 break;
         }
-        // console.log('Dopo:' +  this.posX, this.posY,this.direction);
 
         //Drawing the rect
         context.fillStyle = "black";
@@ -149,11 +138,10 @@ function Snake(width, height, posX, posY) {
         context.strokeStyle = "white";
         context.lineWidth = 0.5;
         context.strokeRect(this.posX, this.posY, this.width, this.height);
-    }
-
+    };
 
     this.die = function () {
-        gameState = false;
+        isGameRunning = false;
 
         this.width = 10;
         this.height = 10;
@@ -165,7 +153,6 @@ function Snake(width, height, posX, posY) {
 
         // Updating records list
         updateRecordsList(current_points);
-
         // Resetto il punteggio
         updatePointsText();
         // Resetto la velocità
@@ -177,30 +164,26 @@ function Snake(width, height, posX, posY) {
         context.fillStyle = "white";
         context.fillRect(this.posX, this.posY, this.width, this.height);
 
-        // sSvuoto l'array contentente i pezzi di coda
         hideTailPieces();
         tailPieces = [];
         food.disappear();
 
         gameOver();
-
-    }
-
+    };
 
     this.checkForBorders = function () {
         if ((this.posX + (this.width / 2)) > canvas.width || (this.posY + (this.height / 2)) > canvas.height ||
             (this.posX + (this.width / 2)) < 0 || (this.posY + (this.height / 2)) < 0) {
             this.die();
         }
-    }
-
+    };
 
     this.foodCollisionDetection = function (foodPosX, foodPosY) {
-        if (this.posX == foodPosX && this.posY == foodPosY) {
+        if (this.posX === foodPosX && this.posY === foodPosY) {
             updatePointsText();
             food.randomSpawn();
-            //Creo un nuovo pezzo di coda
-            if (this.haveTail == false) {
+
+            if (this.haveTail === false) {
                 tailPieces.push(new Tail(this.tailPoisition));
                 this.haveTail = true;
             } else {
@@ -208,12 +191,11 @@ function Snake(width, height, posX, posY) {
             }
             this.tailPoisition++;
 
-            //Playing Sound
             if (game_starting > 2) {
                 playSound("sounds/eat.mp3");
             }
         }
-    }
+    };
 
     this.selfCollisionDetection = function () {
         //Check for collision with his tail pieces
@@ -230,7 +212,6 @@ function Snake(width, height, posX, posY) {
 
 //Snake Tail Class
 function Tail(tailArrPos) {
-
     this.width = snake.width;
     this.height = snake.height;
     this.increment = snake.increment;
@@ -240,26 +221,22 @@ function Tail(tailArrPos) {
         posX: 0,
         posY: 0,
         direction: 'null'
-    }
+    };
 
     this.draw = function () {
-        //Salvo la posizione precedente di questo pezzo di coda
         this.prevPosition.posX = this.posX;
         this.prevPosition.posY = this.posY;
         this.prevPosition.direction = this.direction;
 
-
-        if (this.tailArrPos == 0) { //Se è il primo pezzo di coda
+        if (this.tailArrPos === 0) {
             this.posX = snake.prevPosition['posX'];
             this.posY = snake.prevPosition['posY'];
             this.direction = snake.prevPosition['direction'];
-        } else { // Se NON è il primo pezzo di coda
+        } else {
             this.posX = tailPieces[tailArrPos - 1].prevPosition['posX'];
             this.posY = tailPieces[tailArrPos - 1].prevPosition['posY'];
             this.direction = tailPieces[tailArrPos - 1].prevPosition['posY'];
         }
-        // console.log('Tail: ' + this.posX,this.posY);
-        // console.log('Snake: ' + snake.posX,snake.posY);
 
         switch (this.direction) {
             case 'left':
@@ -275,28 +252,26 @@ function Tail(tailArrPos) {
                 this.posY += this.increment;
                 break;
         }
-        //Drawing the rect
+
+        // Drawing the rect
         context.fillStyle = "black";
         context.fillRect(this.posX, this.posY, this.width, this.height);
         context.strokeStyle = "white";
         context.lineWidth = 0.5;
         context.strokeRect(this.posX, this.posY, this.width, this.height);
-    }
+    };
 
     this.disappear = function () {
-        //Faccio scomparire temporaneamente il cibo
         context.fillStyle = "white";
         context.fillRect(this.posX, this.posY, this.width, this.height);
-    }
+    };
 
     this.foodCollisionDetection = function () {
-        //Check for collision with food
-        if (this.posX == food.PosX && this.posY == food.posY) {
+        if (this.posX === food.posX && this.posY === food.posY) {
             console.log('collision food');
             food.randomSpawn();
         }
-    }
-
+    };
 }
 
 
@@ -308,16 +283,14 @@ function playSound(sound_src) {
     snd.volume = 0.2;
 }
 
-
-//Ricolora lo sfondo del canvas
 function colorBackground() {
     context.fillStyle = "#FFFFFF";
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-//Popola l'array delle posizioni possibili per tenere l'unita di misura scelta, in questo caso 10.
 function populateRandomPos(position_increment) {
-    var single = 0;
+    let single = 0;
+    let randomPos = [];
 
     while (single <= (canvas.width - position_increment)) {
         randomPos.push(single);
@@ -326,10 +299,8 @@ function populateRandomPos(position_increment) {
     return randomPos;
 }
 
-//Aggiorna il punteggio 
 function updatePointsText() {
-
-    if (gameState == true) {
+    if (isGameRunning) {
         current_points++;
 
         if (current_points < 3)
@@ -344,20 +315,18 @@ function updatePointsText() {
 }
 
 function updateRecordsList(entry) {
-    let updateElement = true;
-
     for (let i = 0; i < records.length; i++) {
-        if (entry > records[i] && updateElement) {
+        if (entry === records[i]) {
+            break;
+        } else if (entry > records[i]) {
             records[i] = entry;
-            updateElement = false;
+            recordListElements[i].innerHTML = records[i].toString();
+            break;
         }
-
-        recordListElements[i].innerHTML = records[i].toString();
     }
 }
 
 function hideTailPieces() {
-
     for (var i = 0; i < tailPieces.length; i++) {
         tailPieces[i].disappear();
     }
@@ -365,7 +334,6 @@ function hideTailPieces() {
 
 
 function gameOver() {
-
     context.font = "30px Comic Sans MS";
     context.strokeStyle = "black";
     context.textAlign = "center";
@@ -373,42 +341,42 @@ function gameOver() {
 
     //Restart the game after 2 seconds
     setTimeout(function () {
-        gameState = true;
+        isGameRunning = true;
         mainLoop();
     }, 1800);
 }
 
 /////////////////////////////////////// MAIN LOOP //////////////////////////////////////////////////////////
 
-var snake = new Snake(10, 10, 0, 0);
-var food = new Food(10, 10, 0, 0);
+const snake = new Snake(10, 10, 0, 0);
+const food = new Food(10, 10, 0, 0);
 //food.randomSpawn();
 
 window.addEventListener("keydown", snake.changeDirection);
 
-var FPS = 6;
+let FPS = 6;
 
-//Actual Loop
-var loop = setTimeout(mainLoop, 1000 / FPS);
+// Starting the main game loop
+setTimeout(mainLoop, 1000 / FPS);
 
 function mainLoop() {
 
-    if (gameState == true) {
+    if (isGameRunning) {
         colorBackground();
         //Tail pieces update
         for (var i = 0; i < tailPieces.length; i++) {
             tailPieces[i].draw();
-            // tailPieces[i].foodCollisionDetection();
+            tailPieces[i].foodCollisionDetection();
         }
         i = 0;
 
-        //Funzione che permette di raggiungere i 3 punti canonici all inizio della partita
+        // Starting the game with 3 points as the original game did.
         if (game_starting <= 2) {
             if (game_starting < 2) {
                 food.posX = (snake.posX + snake.increment);
                 food.posY = snake.posY;
             }
-            game_starting++; //Lo faccio arrivare a 3 per vari controlli
+            game_starting++;
         }
 
         food.draw();
